59739dcad5e6ab43f1fe1b7e8e9539b1a1c1e2fe
Jackson Ling <96425149+jacksonllk@users.noreply.github.com>
2024-01-04 20:57:30 +0800

    Update Intro component with social media links

--- components/Intro.js
+++ components/Intro.js
@@ -1,7 +1,12 @@
 // src/app/components/Intro.js
 import Image from "next/image";
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faLinkedin,
+  faSquareXTwitter,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
 import {
   Card,
@@ -15,53 +20,74 @@ import {
 export default function Intro() {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex flex-row justify-between w-full">
+      <div className="flex flex-row justify-between w-auto max-w-[1000px]">
         <div className="p-3">
           <Card className="w-full">
-            <CardHeader>
-              <CardTitle>Hi, I&apos;m Jackson</CardTitle>
-              <CardDescription>
-                I picked up programming mid 2022 and have launched my first
-                online business in October 2023. I run it as a solopreneur for
-                now, but I have benefitted from meeting and learning from people
-                smarter than me on:
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Content</p>
-            </CardContent>
-            <CardFooter>
-              <div className="flex flex-row items-center justify-end gap-x-5">
-                <a
-                  href="https://twitter.com/jacksonllk"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="mr-2"
-                >
-                  <FontAwesomeIcon icon={faSquareXTwitter} />Twitter
-                </a>
-                <a
-                  href="https://linkedin.com/in/jacksonllk"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="mr-2"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} />Linkedin 
-                </a>
+            <div className="flex flex-row">
+              <div>
+                <CardHeader>
+                  <CardTitle>Hi, I&apos;m Jackson</CardTitle>
+                  <CardDescription></CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-row">
+                    <p>
+                      I picked up programming mid 2022 and have launched my first
+                      online business in October 2023. I run it as a solopreneur for
+                      now.
+                    </p>
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <div className="flex flex-row items-center justify-end gap-x-3">
+                    <p>Find me here:</p>
+                    <div className="icon-container">
+                      <Link
+                        href="https://twitter.com/jackson_llk"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FontAwesomeIcon icon={faSquareXTwitter} />
+                      </Link>
+                    </div>
+                    <div className="icon-container">
+                      <Link
+                        href="https://linkedin.com/in/jacksonllk"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FontAwesomeIcon icon={faLinkedin} />
+                      </Link>
+                    </div>
+                  </div>
+                </CardFooter>
               </div>
-            </CardFooter>
+              <div className="p-6 flex items-center">
+          <div className="relative w-full max-w-[200px] h-auto aspect-square rounded-md overflow-hidden sm:max-w-[300px]">
+            <Image
+              src="/images/profile-pic.jpg"
+              alt="Picture of the author"
+              layout="responsive"
+              width={300} // Max width for larger screens
+              height={300} // Ensure square aspect ratio
+              objectFit="cover"
+            />
+          </div>
+        </div>
+            </div>
           </Card>
         </div>
-        <Image
-          src="/images/profile-pic-2.jpg"
-          alt="Picture of the author"
-          width={190}
-          height={290}
-          className="p-3"
-        />
+        {/* <div className="p-3">
+          <div className="relative w-[300px] h-[450px] rounded-md overflow-hidden">
+            <Image
+              src="/images/profile-pic-2.jpg"
+              alt="Picture of the author"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div> */}
       </div>
-
-      <div className="flex flex-row justify-center w-full mt-4"></div>
     </div>
   );
 }
