77213623a1c134b28de8bc978c8a8d7b4d928173
Jackson Ling <jackson.llk@gmail.com>
2024-01-07 15:40:45 +0800

    Remove export command from build script

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx','ts', 'tsx'],
   // Optionally, add any other Next.js config below
+
+    // Add `output: 'standalone'` for deployment
+    output: 'standalone',
 }
  
 module.exports = nextConfig
